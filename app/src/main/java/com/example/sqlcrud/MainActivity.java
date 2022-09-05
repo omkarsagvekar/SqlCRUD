0930a13ec2ea6dc85fa21feef817eb88d3041656
OMKAR <sagvekaromkar940@gmail.com>
2022-09-05 23:17:48 +0530

    Added listener

--- app/src/main/java/com/example/sqlcrud/MainActivity.java
+++ app/src/main/java/com/example/sqlcrud/MainActivity.java
@@ -3,13 +3,66 @@ package com.example.sqlcrud;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.inputmethodservice.KeyboardView;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethod;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnAdmin, btnLogin;
+    private TextView tvCreateUser;
+    private TextInputEditText tiePassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initiaze();
+        allListeners();
+    }
+
+    private void initiaze() {
+        btnAdmin = findViewById(R.id.btn_adminLogin);
+        btnLogin = findViewById(R.id.btn_login);
+        tvCreateUser = findViewById(R.id.tv_linkRegister);
+        tiePassword = findViewById(R.id.tie_password);
+    }
+
+    private void allListeners() {
+        btnAdmin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, AdminLogin.class));
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                startActivity(new Intent(MainActivity.this, ));
+            }
+        });
+        tvCreateUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, CreateLogin.class));
+            }
+        });
+        tiePassword.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean hasFocus) {
+                if(hasFocus){
+                    //
+                }
+            }
+        });
     }
 }
